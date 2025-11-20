b61a30b800bc7b89db62cd33a11c35a589678ac8
kamalesh <kamaleshkoiri300350@gmail.com>
2025-11-20 23:01:13 +0530

    adding uc3

---
+++ snake_ladder_game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class SnakeAndLadderGame
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Snake and Ladder Game\n");
+
+        StartGame();
+
+        int dieValue = rollDie();   
+        CheckOption(dieValue);      
+    }
+
+    // Method to start the game 
+    static void StartGame()
+    {
+        int playerPosition = 0;
+        Console.WriteLine("\nPlayer starts at position: " + playerPosition);
+    }
+
+    // Method to roll a die 
+    static int rollDie()
+    {
+        Random random = new Random();
+        int dieValue = random.Next(1, 7);
+        Console.WriteLine("Player rolled the die and got: " + dieValue);
+        return dieValue;
+    }
+
+    // Method to Check Option
+    static void CheckOption(int dieValue)
+    {
+        Random random = new Random();
+        int option = random.Next(0, 3);   
+
+        if (option == 0)
+        {
+            Console.WriteLine("Option: No Play → Player stays in the same position.");
+        }
+        else if (option == 1)
+        {
+            Console.WriteLine("Option: Ladder → Player moves ahead by " + dieValue);
+        }
+        else
+        {
+            Console.WriteLine("Option: Snake → Player moves behind by " + dieValue);
+        }
+    }
+}
